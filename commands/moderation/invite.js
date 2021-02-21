2edb0430bf66a4785f1787fb3d7c61619f12c8d8
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 20:59:13 +0530

    Create invite.js

---
+++ commands/moderation/invite.js
@@ -0,0 +1,8 @@
+module.exports = ({
+ name: "invite", 
+ code: `$title[Invite?]
+$description[Hey wanna invite me? 
+**Then just one tap and go!** [Click Here\\](https://discord.com/oauth2/authorize?client_id=780342089120612373&scope=bot&permissions=2147483647)]
+$color[000001]
+$cooldown[5;]`
+});
