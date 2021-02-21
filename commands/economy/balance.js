343f848969e92c775de37eed2a493b8f29391866
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 16:40:06 +0530

    Create balance.js

---
+++ commands/economy/balance.js
@@ -0,0 +1,9 @@
+module.exports = ({
+  name: "balance", aliases: ['bal'], 
+  code: `$title[Balance]
+  $description[**Pocket: $getGlobalUserVar[Money] coins
+Bank:    $getGlobalUserVar[Bank] coins**]
+$addTimestamp
+$color[ff2050]
+$thumbnail[https://cdn.discordapp.com/attachments/797833158782353428/802171573158936616/IMG_20210122_191238.jpg]`
+ });
