3e883b69d0ecae29966f8c263c9c9b4fad358da7
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 20:57:05 +0530

    Create bot.js

---
+++ commands/moderation/bot.js
@@ -0,0 +1,10 @@
+module.exports = ({
+name: "<@780342089120612373>",
+nonPrefixed: true,
+code: `
+  $title[I GOT A PING]
+$description[Hello,My prefix is \`a?\` or you can type \`help\` for any help.]
+$color[RANDOM]
+$cooldown[10m]
+`
+})
