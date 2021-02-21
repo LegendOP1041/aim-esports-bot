323110164df9b3030fe791aafa9ca20d822db821
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 21:10:37 +0530

    Create status.js

---
+++ commands/others/status.js
@@ -0,0 +1,6 @@
+module.exports = ({
+  name: "setStatus", 
+  code: `$setStatus[$message[3] $message[4] $message[5] $message[6] $message[7] $message[8],$message[1],$message[2]]
+  Successfully Changed the bot status
+  $onlyForIDs[510349473420017666;]`
+  });
