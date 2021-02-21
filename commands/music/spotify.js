8f3aedc3c5cb586140b9fbb16b6f805b32083fbe
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 21:20:54 +0530

    Create spotify.js

---
+++ commands/music/spotify.js
@@ -0,0 +1,6 @@
+module.exports=({
+name: "spotify",
+code: `$description[**Spotify** <:spotify:801282281234890752>
+$playSpotify[https://open.spotify.com/playlist/39Ly1GToXwzdzWtkH9Befc?si=irltrxEhSwO3gt-xCAR4qQ;name;An error occurred, make sure the spotify link is valid & you have joined a voice channel]]
+$color[000001] $suppressErrors[Make sure that u r in a voice channel] $thumbnail[$serverIcon]`
+});
