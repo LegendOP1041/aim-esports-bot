fbacadfacfcecdff89cc263cfec6e10e6c1fddb6
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 20:58:50 +0530

    Create info.js

---
+++ commands/moderation/info.js
@@ -0,0 +1,18 @@
+module.exports = ({
+  name: "info", 
+  code: `$color[000001]
+$title[Bot info]
+$description[<:developer:787232988164390913> **Developer: LegendOP**
+
+<:Live:787233820214165574> **Live in: $serverCount Servers**
+
+<:online_globe:787236977741135882> **Memory usage: $randomText[18;35;38;25;67;88;81;92;27;35;86;91;81;55]mb**
+
+<a:onlinex:787235116375736321> **Cpu usage: $cpu**
+
+<a:onlinex:787235116375736321> **Ram usage: $rammb**
+
+<:download:787234406371950602> **Ping: $pingms**
+
+[Invite me\\](https://discord.com/oauth2/authorize?client_id=780342089120612373&scope=bot&permissions=2147483647) | [Support\\](https://discord.gg/HeFKTPmeMh) <:dc_support:788068298494705685>]`
+});
