bbe6434eda63f42a069fa2283be855322a0ef364
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 21:06:25 +0530

    Create dm.js

---
+++ commands/others/dm.js
@@ -0,0 +1,12 @@
+module.exports = ({
+  name: "dm", 
+  code: `$color[000001]
+$dm[$splitText[1]]
+$description[**<a:Mail56:795883795161153538> Mail From $username**
+<a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390><a:aUE_SingleLine:795880231600783390>
+$splitText[2]]
+$image[$splitText[3]]
+$textSplit[$message;|]
+$argsCheck[>1;Pls mention someone whom you want to DM.]
+$argsCheck[>2;Pls type the message]`
+  });
