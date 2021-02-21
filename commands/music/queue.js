af63e9f27f3ded281be7c871a581cbebd4beb426
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 21:14:53 +0530

    Create queue.js

---
+++ commands/music/queue.js
@@ -0,0 +1,6 @@
+module.exports = ({
+  name: "queue", aliases: ['q'], 
+  code: `$description[$queue[1;10;{number} - {title} by <@{userID}>]
+$color[000001]]
+$title[Queue] $thumbnail[$serverIcon]`
+});
