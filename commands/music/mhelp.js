5dc5840d87bf09f0b44ab9449d9be52bd4b15da4
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 21:17:28 +0530

    Create mhelp.js

---
+++ commands/music/mhelp.js
@@ -0,0 +1,31 @@
+module.exports=({
+name: "mhelp",
+code: `$title[Music Help]
+$thumbnail[$userAvatar[800604193328332850]] $description[**Play ▶**
+To Play Music
+
+**Pause ⏸**
+To Pause Music
+
+**Resume ▶️**
+To Resume Music
+
+**Stop ⏹**
+To stop music
+
+**Spotify <:spotify:801282281234890752>**
+To Play Spotify
+
+**Loop 🔃**
+To Play Music In Loop
+
+**Lyrics 🎶**
+To Get Music Lyrics
+
+**Skip ⏩**
+To Skip The Current
+
+**Queue 📄**
+To Get Songs List
+
+**Volume 🔊**
