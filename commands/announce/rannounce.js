270abc86d65ca3031f6236bed7d20d4b3fb19ccf
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 16:38:10 +0530

    Create rannounce.js

---
+++ commands/announce/rannounce.js
@@ -0,0 +1,8 @@
+module.exports = ({
+  name: "rannounce", 
+  code: `$setServerVar[an;]
+$title[ANNOUNCEMEMT DISABLED]
+$color[#ff2052]
+$description[**ANNOUNCEMEMT IS DISABLED BY $username**]
+`
+ });
