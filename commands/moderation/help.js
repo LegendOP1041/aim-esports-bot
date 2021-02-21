2d49755b36b3d94c9575769b9ece875df129433a
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 20:58:00 +0530

    Create help.js

---
+++ commands/moderation/help.js
@@ -0,0 +1,31 @@
+module.exports = ({
+  name: "help", 
+  code: `$dm
+  $deletecommand
+$color[000001]
+$description[<:help183:795912076265652255> **| HELP COMMANDS**
+
+<a:online_768:787235116375736321> **| MISCELLANEOUS**
+\`help, info, support, embed, av, invite, serverinfo, servericon, whois, updates\`
+
+<:Music:805293573858787350> **| MUSIC**
+\`play, stop, queue, skip, skipto, volume, loop, lyrics, spotify, pause, resume\`
+
+<a:ytien_477r:785328953283444767> **| MODERATION**
+\`kick, ban, mute, unmute, servermute, warn, check warn, clear warn, lockdown, unlock, dm, purge, giverole, takerole, addemoji, +cc(create channel), afk on, afk off, poll, antilink on, antilink off\`
+
+<:member29:795910455612801024> **| ROLE SYSTEM**
+\`addwelcomerole(will be add soon)\`
+
+🎟 **| TICKET SYSTEM**
+\`ticket, closeticket, thelp\`
+
+<a:OP_Gif:787904340555202560> **| USEFULL COMMANDS**
+\`date, day, setannounce, announce, rannounce, ping, BotLeave, emojis\`
+
+📊 **| RANK**
+\`rank, resetrank, setrank <channel>, setrankmsg\`
+
+<:Coin36:797812820585611264> **| ECONOMY**
+\`bal, dep, with, work, beg, daily, weekly, shop, item\`]
+$suppressErrors
