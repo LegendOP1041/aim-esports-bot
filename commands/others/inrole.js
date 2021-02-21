85fe72a9f1e053f03c04dee7a408a46e1bbc1c62
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 21:07:28 +0530

    Create inrole.js

---
+++ commands/others/inrole.js
@@ -0,0 +1,9 @@
+module.exports = ({
+  name: "inrole",
+  code: `$title[Inrole]
+$description[$usersWithRole[$roleID[$message];
+]]
+$onlyPerms[managemessages;]
+$suppressErrors[Correct Ussage: a?inrole <role name>]`
+});
+// Will return: Leref|Ruben
