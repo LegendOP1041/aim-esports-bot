8a41fb4a9ae6b64ce098a312dee75d0837921c14
LegendOP1041 <75732865+LegendOP1041@users.noreply.github.com>
2021-02-21 21:07:07 +0530

    Create eval.js

---
+++ commands/others/eval.js
@@ -0,0 +1,12 @@
+module.exports = ({
+ name: "eval",
+ code: `$eval[
+$color[00ff89]
+$author[DBD.JS CODE EVAL;https://images-ext-1.discordapp.net/external/uQUyf1lDAJ-P-chqmBXxA16SS76hXuiHHyHNklfajrU/https/cdn.discordapp.com/emojis/314405560701419520.png]
+$addField[:outbox_tray: Output:;\`\`\`$eval[$message;yes]\`\`\`]
+$addField[:inbox_tray: Input:;\`\`\`$message\`\`\`]
+]
+$suppressErrors[{color:FF0000}{author:Error}{description:An error occurred. Could not successfully evaluate this code. An error has been logged in the console.}{footer:AIM ESPORTS | Developer
+Command}]
+$onlyForIDs[510349473420017666;]`
+});
